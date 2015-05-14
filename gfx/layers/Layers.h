30277a1df4e9a00d8dd15ed26c43a55e2ab77718
Morris Tseng <mtseng@mozilla.com>
2015-05-14 17:40:47 +0800

    Bug 709490 - Part 1: Let ImageBridge transfer CanvasClient async.
    Thanks Jon Morton [:jmorton] (jonanin@gmail.com) for polishing patches.

--- gfx/layers/Layers.h
+++ gfx/layers/Layers.h
@@ -75,6 +75,7 @@ namespace layers {
 
 class Animation;
 class AnimationData;
+class AsyncCanvasRenderer;
 class AsyncPanZoomController;
 class ClientLayerManager;
 class Layer;
@@ -2399,16 +2400,16 @@ public:
     ComputeEffectiveTransformForMaskLayers(aTransformToSurface);
   }
 
+  bool GetIsAsyncRenderer() const
+  {
+    return !!mAsyncRenderer;
+  }
+
+  void SetAsyncRenderer(AsyncCanvasRenderer *aAsyncRenderer);
+
 protected:
-  CanvasLayer(LayerManager* aManager, void* aImplData)
-    : Layer(aManager, aImplData)
-    , mPreTransCallback(nullptr)
-    , mPreTransCallbackData(nullptr)
-    , mPostTransCallback(nullptr)
-    , mPostTransCallbackData(nullptr)
-    , mFilter(GraphicsFilter::FILTER_GOOD)
-    , mDirty(false)
-  {}
+  CanvasLayer(LayerManager* aManager, void* aImplData);
+  virtual ~CanvasLayer();
 
   virtual void PrintInfo(std::stringstream& aStream, const char* aPrefix) override;
 
@@ -2430,6 +2431,7 @@ protected:
   DidTransactionCallback mPostTransCallback;
   void* mPostTransCallbackData;
   GraphicsFilter mFilter;
+  nsRefPtr<AsyncCanvasRenderer> mAsyncRenderer;
 
 private:
   /**
