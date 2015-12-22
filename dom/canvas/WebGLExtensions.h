6048a20d17798f6ac319604967ad5cdf3043197e
Morris Tseng <mtseng@mozilla.com>
2015-12-22 09:22:27 +0800

    Bug 917505 - Add WEBGL_compressed_texture_es3 support. r=jgilbert

--- dom/canvas/WebGLExtensions.h
+++ dom/canvas/WebGLExtensions.h
@@ -71,6 +71,16 @@ public:
     DECL_WEBGL_EXTENSION_GOOP
 };
 
+class WebGLExtensionCompressedTextureES3
+    : public WebGLExtensionBase
+{
+public:
+    WebGLExtensionCompressedTextureES3(WebGLContext*);
+    virtual ~WebGLExtensionCompressedTextureES3();
+
+    DECL_WEBGL_EXTENSION_GOOP
+};
+
 class WebGLExtensionCompressedTextureETC1
     : public WebGLExtensionBase
 {
