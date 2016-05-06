c4982ac318dfdf4097a7180f8ca8f03404d69be0
Morris Tseng <mtseng@mozilla.com>
2016-05-06 10:43:39 +0800

    Bug 1254400 - Make access to d3d9 device manager thread-safe. r=dvander

--- gfx/thebes/gfxWindowsPlatform.cpp
+++ gfx/thebes/gfxWindowsPlatform.cpp
@@ -1438,6 +1438,7 @@ void
 gfxWindowsPlatform::OnDeviceManagerDestroy(DeviceManagerD3D9* aDeviceManager)
 {
   if (aDeviceManager == mDeviceManager) {
+    MutexAutoLock lock(mDeviceLock);
     mDeviceManager = nullptr;
   }
 }
@@ -1445,7 +1446,7 @@ gfxWindowsPlatform::OnDeviceManagerDestroy(DeviceManagerD3D9* aDeviceManager)
 IDirect3DDevice9*
 gfxWindowsPlatform::GetD3D9Device()
 {
-  DeviceManagerD3D9* manager = GetD3D9DeviceManager();
+  RefPtr<DeviceManagerD3D9> manager = GetD3D9DeviceManager();
   return manager ? manager->device() : nullptr;
 }
 
@@ -1454,11 +1455,13 @@ gfxWindowsPlatform::D3D9DeviceReset() {
   mHasD3D9DeviceReset = true;
 }
 
-DeviceManagerD3D9*
+already_AddRefed<DeviceManagerD3D9>
 gfxWindowsPlatform::GetD3D9DeviceManager()
 {
   // We should only create the d3d9 device on the compositor thread
   // or we don't have a compositor thread.
+  RefPtr<DeviceManagerD3D9> result;
+  MutexAutoLock lock(mDeviceLock);
   if (!mDeviceManager &&
       (!gfxPlatform::UsesOffMainThreadCompositing() ||
        CompositorParent::IsInCompositorThread())) {
@@ -1469,7 +1472,8 @@ gfxWindowsPlatform::GetD3D9DeviceManager()
     }
   }
 
-  return mDeviceManager;
+  result = mDeviceManager;
+  return result.forget();
 }
 
 ID3D11Device*
