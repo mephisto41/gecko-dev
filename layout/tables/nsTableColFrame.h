6b25486e0f630fe9fc8845560143ac940e81a04c
Morris Tseng <mtseng@mozilla.com>
2016-07-18 23:59:34 -0400

    Bug 929484 - Draw each table's background on their own display item. r=mstange
    This patch do following things:
    1. Create nsDisplayTableBorderCollapse that draws all collapse border of
    table.
    2. Don't use nsDisplayTableBorderBackground.
    3. Let column and column group frame generate display items.
    4. When traversing the table, also traverse the column and column group
    frames.
    5. For each type of table frame (col group, col, row group, row and
    cell), draw their own background.
    
    MozReview-Commit-ID: 1s2VLv6G8xi

--- layout/tables/nsTableColFrame.h
+++ layout/tables/nsTableColFrame.h
@@ -52,12 +52,9 @@ public:
                       const ReflowInput& aReflowInput,
                       nsReflowStatus&          aStatus) override;
 
-  /**
-   * Table columns never paint anything, nor receive events.
-   */
   virtual void BuildDisplayList(nsDisplayListBuilder*   aBuilder,
                                 const nsRect&           aDirtyRect,
-                                const nsDisplayListSet& aLists) override {}
+                                const nsDisplayListSet& aLists) override;
 
   /**
    * Get the "type" of the frame
