73cd099ffd23214ea01a072f972ea3aa825a6cec
Morris Tseng <mtseng@mozilla.com>
2017-05-12 11:23:39 +0800

    Bug 1354463 - Enable layers.advanced.canvas-background-color is webrender is enabled. r=kats
    MozReview-Commit-ID: KrJY8i9ms42

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -5709,6 +5709,7 @@ pref("fuzzing.enabled", false);
 pref("layers.advanced.border-layers", 2);
 pref("layers.advanced.boxshadow-inset-layers", 2);
 pref("layers.advanced.boxshadow-outer-layers", 2);
+pref("layers.advanced.canvas-background-color", 2);
 pref("layers.advanced.caret-layers", 2);
 pref("layers.advanced.displaybuttonborder-layers", 2);
 pref("layers.advanced.outline-layers", 2);
